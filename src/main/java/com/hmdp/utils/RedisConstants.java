a3b521288d3e7446d9a1393972d8a54cc7720a76
hfnu_2010211112 <2942885033@qq.com>
2023-10-05 17:56:43 +0800

    feat:redis数据类型geo地图搜索单元测试

--- src/main/java/com/hmdp/utils/RedisConstants.java
+++ src/main/java/com/hmdp/utils/RedisConstants.java
@@ -59,6 +59,11 @@ public class RedisConstants {
     public static final String SECKILL_STOCK_KEY = "seckill:stock:";
     public static final String BLOG_LIKED_KEY = "blog:liked:";
     public static final String FEED_KEY = "feed:";
+
+    /**
+     * geo 搜索路径 redis key
+     */
     public static final String SHOP_GEO_KEY = "shop:geo:";
+
     public static final String USER_SIGN_KEY = "sign:";
 }
--- src/test/java/com/hmdp/HmDianPingApplicationTests.java
+++ src/test/java/com/hmdp/HmDianPingApplicationTests.java
@@ -1,9 +1,59 @@
 package com.hmdp;
 
+import com.hmdp.entity.Shop;
+import com.hmdp.service.IShopService;
+import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.data.geo.Point;
+import org.springframework.data.redis.connection.RedisGeoCommands;
+import org.springframework.data.redis.core.StringRedisTemplate;
+
+import javax.annotation.Resource;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Map;
+import java.util.stream.Collectors;
+
+import static com.hmdp.utils.RedisConstants.SHOP_GEO_KEY;
 
 @SpringBootTest
 class HmDianPingApplicationTests {
 
+    @Resource
+    private IShopService shopService;
+
+    @Resource
+    private StringRedisTemplate stringRedisTemplate;
+
+    /**
+     * 导入店铺数据到 geo
+     */
+    @Test
+    void importData(){
+        //查询数据
+        List<Shop> shopList = shopService.list();
+
+        // 按typeId分组
+        Map<Long, List<Shop>> shopMap = shopList.stream().collect(Collectors.groupingBy(Shop::getTypeId));
+
+        // 依次存入到redis
+        for (Map.Entry<Long, List<Shop>> shopEntry : shopMap.entrySet()) {
+            //获取类型id
+            Long typeId = shopEntry.getKey();
+            List<Shop> value = shopEntry.getValue();
+
+            List<RedisGeoCommands.GeoLocation<String>> locations = new ArrayList<>(value.size());
+
+            //获取店铺集合
+            for (Shop shop : value) {
+                // 每一条附近店铺信息写入redis
+//                stringRedisTemplate.opsForGeo().add(SHOP_GEO_KEY + typeId, new Point(shop.getX(), shop.getY()), shop.getId().toString());
+
+                locations.add(new RedisGeoCommands.GeoLocation<>(shop.getId().toString(),
+                        new Point(shop.getX(), shop.getY())));
+            }
+            stringRedisTemplate.opsForGeo().add(SHOP_GEO_KEY + typeId,locations);
+        }
+    }
 
 }
