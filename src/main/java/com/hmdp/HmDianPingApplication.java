f363b017bc8cb8899a521eeb450f8803dd9c88c5
hfnu_2010211112 <2942885033@qq.com>
2023-12-23 19:52:31 +0800

    feat:引入swagger集成springboot技术

--- src/main/java/com/hmdp/HmDianPingApplication.java
+++ src/main/java/com/hmdp/HmDianPingApplication.java
@@ -1,17 +1,33 @@
 package com.hmdp;
 
+import lombok.extern.slf4j.Slf4j;
 import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ConfigurableApplicationContext;
 import org.springframework.context.annotation.EnableAspectJAutoProxy;
+import org.springframework.core.env.ConfigurableEnvironment;
 
+import java.net.InetAddress;
+import java.net.UnknownHostException;
+
+@Slf4j
 @EnableAspectJAutoProxy(exposeProxy = true)
 @MapperScan("com.hmdp.mapper")
 @SpringBootApplication
 public class HmDianPingApplication {
 
-    public static void main(String[] args) {
-        SpringApplication.run(HmDianPingApplication.class, args);
+    public static void main(String[] args) throws UnknownHostException {
+        ConfigurableApplicationContext application = SpringApplication.run(HmDianPingApplication.class, args);
+        ConfigurableEnvironment env = application.getEnvironment();
+        String ip = InetAddress.getLocalHost().getHostAddress();
+        String port = env.getProperty("server.port");
+        log.info("\n----------------------------------------------------------\n\t" +
+                "Application hmdp is running! Access URLs:\n\t" +
+                "Local: \t\thttp://localhost:" + port + "/\n\t" +
+                "External: \thttp://" + ip + ":" + port + "/\n\t" +
+                "Swagger文档: \thttp://" + ip + ":" + port + "/doc.html\n" +
+                "----------------------------------------------------------");
     }
 
 }
